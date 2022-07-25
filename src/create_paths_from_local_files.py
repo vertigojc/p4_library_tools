20c75e5089590bf847c07acfbf40deaf9be78863
Jase Lindgren <jclindgren@gmail.com>
2022-07-25 14:03:08 -0700

    Adding extra explanation to output

--- src/create_paths_from_local_files.py
+++ src/create_paths_from_local_files.py
@@ -20,20 +20,26 @@ LOCAL_LIBRARY_PATH = "UE/Content/Library"
 p4 = P4.P4()
 p4.connect()
 
+
 def main():
     workspace_root = get_root()
     local_library = workspace_root / LOCAL_LIBRARY_PATH
     local_folders = get_local_library_folders(local_library)
     shares, imports = get_shares_and_imports(local_folders)
+    print_results(shares, imports)
 
+
+def print_results(shares, imports):
+    print("Copy either the Share or Import paths below into your library dev stream.")
+    print()
     print("Share paths: (preferred for child streams of main library)")
-    for share in shares:
+    for share in sorted(shares):
         print(f"share {share}")
     print()
-    print()
     print("Import paths: (use for non-child streams)")
-    for imp in imports:
+    for imp in sorted(imports):
         print(f"import+ {imp}")
+
 
 def get_local_library_folders(local_library):
     """Returns a Set of all unique directories in the local workspace's library folder"""
@@ -44,6 +50,7 @@ def get_local_library_folders(local_library):
 def get_shares_and_imports(local_folders):
     shares = [f"{MAIN_LIBRARY_PATH}/{folder.as_posix()}/..." for folder, parent_count in local_folders.items() if parent_count == 0 ]
     imports = [f"{MAIN_LIBRARY_BRANCH}/{MAIN_LIBRARY_PATH}/{folder.as_posix()}/..." for folder, parent_count in local_folders.items() if parent_count == 0]
+    return shares, imports
 
 
 def get_root():
